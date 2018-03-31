8edd16e0e51413e89991d2a944170956583026fa
DeXoteric <marlev@poczta.pl>
2018-03-31 22:07:29 +0200

    initial commit

---
+++ app/src/main/java/com/dexoteric/madalchemist/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.dexoteric.madalchemist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        onWindowFocusChanged(true)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (hasFocus) {
+            systemUI() // UI immersive sticky
+        }
+    }
+
+    // UI immersive sticky
+    private fun systemUI() {
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        window.decorView.systemUiVisibility = (flags)
+    }
+
+}
