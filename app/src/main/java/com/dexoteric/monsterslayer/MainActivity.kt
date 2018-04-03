32a7e00b3603a03d32016eaca5ecec7d00e0ac00
DeXoteric <marlev@poczta.pl>
2018-04-03 21:25:58 +0200

    boss arrays

--- app/src/main/java/com/dexoteric/monsterslayer/MainActivity.kt
+++ app/src/main/java/com/dexoteric/monsterslayer/MainActivity.kt
@@ -3,30 +3,42 @@ package com.dexoteric.monsterslayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    val arrayBossNames = resources.getStringArray(R.array.boss_name)
-    val arrayBossPrefix = resources.getStringArray(R.array.boss_prefix)
-    val arrayBossSufix = resources.getStringArray(R.array.boss_sufix)
-    
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         onWindowFocusChanged(true)
+
+        // inicjalizacja Array
+        val arrayBossNames: Array<String> = resources.getStringArray(R.array.boss_name)
+        val arrayBossPrefix: Array<String> = resources.getStringArray(R.array.boss_prefix)
+        val arrayBossSuffix: Array<String> = resources.getStringArray(R.array.boss_suffix)
+
+        // inicjalizacja TextView
+        val testText :TextView = findViewById(R.id.textView)
+
+        // przypisuje do zmiennych String z zadeklarowanych Array<String> z pozycji [x] oraz wyświetla całość w zadeklarowanym TextView
+        val bossName = arrayBossNames[0]
+        val bossPrefix = arrayBossPrefix[0]
+        val bossSuffix = arrayBossSuffix[0]
+        val boss = "$bossPrefix $bossName $bossSuffix"
+        testText.text = boss
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
 
         if (hasFocus) {
-            systemUI() // UI immersive sticky
+            // przywołanie funckji
+            systemUI()
         }
     }
 
-    // UI immersive sticky
+    // funkcja odpowiadająca za ustawienie flag full_screen
     private fun systemUI() {
         val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
